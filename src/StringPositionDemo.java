7315b0483c37756345b8dde7dd1a0f8d0e64476c
Hajar <hajar2000alfarsi@gmail.com>
2026-04-19 11:33:25 +0400

    Add Task 4: Find Positions and Check String Start or End in Java

---
+++ src/StringPositionDemo.java
@@ -0,0 +1,11 @@
+public class StringPositionDemo {
+    public static void main(String[] args) {
+        String text="Java Programming";
+        System.out.println("Original text: " + text);
+        System.out.println("index of 'P': " + text.indexOf('P'));
+        System.out.println("Does text start with 'J': " + text.startsWith("J"));
+        System.out.println("Does text ends with 'Programming': " + text.endsWith("Programming"));
+
+
+    }
+}
